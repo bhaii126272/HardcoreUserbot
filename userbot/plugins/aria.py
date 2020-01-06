dbfe1f3d9dc582214e05c90149d24dbeb6750af3
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-06 23:57:10 +0530

    Update aria.py

--- userbot/plugins/aria.py
+++ userbot/plugins/aria.py
@@ -13,6 +13,7 @@ import aria2p
 from telethon import events
 import asyncio
 import os
+from userbot.utils import admin_cmd
 
 
 cmd = "aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800  --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true --allow-overwrite=true"
@@ -28,8 +29,8 @@ aria2 = aria2p.API(
 	)
 
 
-@borg.on(events.NewMessage(pattern=r"\.magnet", outgoing=True))
-async def magnet_download(event):
+@borg.on(admin_cmd(pattern=r"magnet"))
+async def magnet_download(event);
 	if event.fwd_from:
 		return
 	var = event.text
@@ -52,7 +53,7 @@ async def magnet_download(event):
 	
 
 
-@borg.on(events.NewMessage(pattern=r"\.tor", outgoing=True))
+@borg.on(admin_cmd(pattern=r"tor"))
 async def torrent_download(event):
 	if event.fwd_from:
 		return
@@ -68,7 +69,7 @@ async def torrent_download(event):
 	gid = download.gid
 	await progress_status(gid=gid,event=event,previous=None)
 
-@borg.on(events.NewMessage(pattern=r"\.url", outgoing=True))
+@borg.on(admin_cmd(pattern=r"url"))
 async def magnet_download(event):
 	if event.fwd_from:
 		return
@@ -88,7 +89,7 @@ async def magnet_download(event):
 		new_gid = await check_metadata(gid)
 		await progress_status(gid=new_gid,event=event,previous=None)
 
-@borg.on(events.NewMessage(pattern=r"\.ariaRM", outgoing=True))
+@borg.on(admin_cmd(pattern=r"ariaRM"))
 async def remove_all(event):
 	if event.fwd_from:
 		return
@@ -101,7 +102,7 @@ async def remove_all(event):
 		os.system("aria2p remove-all")
 	await event.edit("`Removed All Downloads.`")  
 
-@borg.on(events.NewMessage(pattern=r"\.show", outgoing=True))
+@borg.on(admin_cmd(pattern=r"show"))
 async def show_all(event):
 	if event.fwd_from:
 		return
