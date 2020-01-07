31b91601aed687c8adeffa02e50c2f797821149a
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-07 22:42:22 +0530

    Update randomownsticker.py

--- userbot/plugins/randomownsticker.py
+++ userbot/plugins/randomownsticker.py
@@ -28,8 +28,8 @@ def choser(cmd, pack, blacklist={}):
         await event.respond(file=random.choice(docs))
 
 
-choser('rstic', '@Halto_Tha ki gufa me')
-choser('ranstic', 'Jayu\'s', {
+choser('rstic', 'JayukeStickers')
+choser('ranstic', 'JayukeAnimated', {
     1653974154589768377,
     1653974154589768312,
     1653974154589767857,
