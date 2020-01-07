5939da0923fa6894af09414bb365827440468acf
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-07 16:30:45 +0530

    Update ding.py

--- userbot/plugins/ding.py
+++ userbot/plugins/ding.py
@@ -8,11 +8,11 @@ from telethon import events
 
 import asyncio
 
+from userbot.utils import admin_cmd
 
 
 
-
-@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+@borg.on(admin_cmd(pattern=r"(.*)"))
 
 async def _(event):
 
