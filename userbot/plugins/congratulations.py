3fc0ef727b40f7a5156361d94e25812cbb7180e4
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-10 22:39:31 +0530

    Create congratulations.py

---
+++ userbot/plugins/congratulations.py
@@ -0,0 +1,25 @@
+from telethon import events
+import random, re
+from uniborg.util import admin_cmd
+
+RUNSREACTS = [
+    "`Runs to Thanos`",
+    "`Runs far, far away from earth`",
+    "`Running faster than usian bolt coz I'mma Bot`",
+    "`Runs to Marie`",
+    "`This Group is too cancerous to deal with.`",
+    "`Cya bois`",
+    "`I am a mad person. Plox Ban me.`",
+    "`I go away`",
+    "`I am just walking off, coz me is too fat.`",
+    "`I Fugged off!`",
+]
+
+@borg.on(admin_cmd(pattern="congo"))
+async def _(event):
+    if event.fwd_from:
+         return
+    bro = random.randint(0, len(RUNSREACTS) - 1)    
+    input_str = event.pattern_match.group(1)
+    reply_text = RUNSREACTS[bro]
+    await event.edit(reply_text)
