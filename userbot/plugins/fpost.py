6c4d1b322e16428481b8a816a96c41e4bd251806
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-11 10:59:00 +0530

    Update fpost.py

--- userbot/plugins/fpost.py
+++ userbot/plugins/fpost.py
@@ -9,11 +9,12 @@ import string
 
 from telethon import events
 from telethon.tl import types
+from uniborg.util import admin_cmd
 
 msg_cache = {}
 
 
-@borg.on(events.NewMessage(pattern=r"\.fpost\s+(.*)", outgoing=True))
+@borg.on(admin_cmd(pattern=r"\.fpost\s+(.*)"))
 async def _(event):
     await event.delete()
     text = event.pattern_match.group(1)
