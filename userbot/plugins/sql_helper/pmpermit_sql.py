7b877131305ac6fd795ca84e94af856a0f7c3987
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-11 22:01:46 +0530

    Update pmpermit_sql.py

--- userbot/plugins/sql_helper/pmpermit_sql.py
+++ userbot/plugins/sql_helper/pmpermit_sql.py
@@ -1,16 +1,15 @@
-try:
-    from userbot.modules.sql_helper import SESSION, BASE
-except ImportError:
-    raise Exception("Hello!")
-from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
+from sqlalchemy import Column, String
+from userbot.plugins.sql_helper import SESSION, BASE
 
 
 class PMPermit(BASE):
     __tablename__ = "pmpermit"
     chat_id = Column(String(14), primary_key=True)
+    reason = Column(String(127))
 
-    def __init__(self, chat_id):
-        self.chat_id = str(chat_id)  # ensure string
+    def __init__(self, chat_id, reason=""):
+        self.chat_id = chat_id
+        self.reason = reason
 
 
 PMPermit.__table__.create(checkfirst=True)
@@ -25,14 +24,20 @@ def is_approved(chat_id):
         SESSION.close()
 
 
-def approve(chat_id):
-    adder = PMPermit(str(chat_id))
+def approve(chat_id, reason):
+    adder = PMPermit(str(chat_id), str(reason))
     SESSION.add(adder)
     SESSION.commit()
 
 
-def dissprove(chat_id):
+def disapprove(chat_id):
     rem = SESSION.query(PMPermit).get(str(chat_id))
     if rem:
         SESSION.delete(rem)
         SESSION.commit()
+
+
+def get_all_approved():
+    rem = SESSION.query(PMPermit).all()
+    SESSION.close()
+    return rem
