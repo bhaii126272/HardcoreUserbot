2584505597a31f0354a858637b9125e6a7e466b4
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-24 09:38:07 +0530

    Update sangmata.py

--- userbot/plugins/sangmata.py
+++ userbot/plugins/sangmata.py
@@ -25,7 +25,8 @@ async def _(event):
     async with borg.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
-              await borg.forward_messages(chat, reply_message)
+             # await borg.forward_messages(chat, reply_message)
+              await silently_send_message(chat,"/generate")
               response = await response 
           except YouBlockedUserError: 
               await event.reply("```Please unblock @sangmatainfo_bot and try again```")
