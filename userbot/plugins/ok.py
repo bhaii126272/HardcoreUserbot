e54ddc03d2ea37c13fe4b3667b64735d276b7f7e
japarmar <55135328+japarmar@users.noreply.github.com>
2020-01-25 20:50:03 +0530

    Update ok.py

--- userbot/plugins/ok.py
+++ userbot/plugins/ok.py
@@ -10,16 +10,16 @@ import asyncio
 
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd("(.*)"))
+@borg.on(admin_cmd("ok"))
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.00001
-    animation_ttl = range(0, 90)
+    animation_interval = 0.1
+    animation_ttl = range(0,36)
     input_str = event.pattern_match.group(1)
-    if input_str == "ok":
-        await event.edit(input_str)
-        animation_chars = [
+   # if input_str == "ok":
+    await event.edit(input_str)
+    animation_chars = [
             "F",
             "U",
             "C",
@@ -40,7 +40,7 @@ async def _(event):
             "Ok Sar 😇"
         ]
 
-        for i in animation_ttl:
+    for i in animation_ttl:
         	
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 18])
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 18])
