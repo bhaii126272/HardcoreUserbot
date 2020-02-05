8ecf57aa61306725d23c5ce61e3ae3425286a412
Hack12R <60672741+Hack12R@users.noreply.github.com>
2020-02-06 00:26:15 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -898,7 +898,7 @@ async def vapor(vpr):
 @register(outgoing=True, pattern="^.repo$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Click [here](https://github.com/Hack12R/HardcoreUserbot) to open this lit af repo.")
+        await e.edit("Click [here](https://github.com/Hack12R/HardcoreUserbot) to open this lit af repo by @Hack12R.")
 			  
 			  
 @register(outgoing=True, pattern="^.str(?: |$)(.*)")
